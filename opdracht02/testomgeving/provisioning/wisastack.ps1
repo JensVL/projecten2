1ffe816d3a136dbcb9608a26ee0c6843989ad8d9
Matthias Van De Velde <fpkmatthi0@gmail.com>
2019-03-11 16:44:39 +0100

    WISA - reworked provisioning

--- opdracht02/testomgeving/provisioning/wisastack.ps1
+++ opdracht02/testomgeving/provisioning/wisastack.ps1
@@ -4,19 +4,55 @@ Param(
   [String]$rootpassword = "root",
   [int]$tcpportnr = 50000,
   [String]$dbname = "vagrant",
-  [String]$username = "vagrant",
-  [String]$password = "vagrant"
+  [String]$sqlusername = "vagrant",
+  [String]$sqlpassword = "vagrant",
+
+  [string]$iisusername = "vagrant",
+  [String]$iispassword = "vagrant",
+
+  [string]$downloadpath = "C:\SetupMedia",
+  [boolean]$asp35 = $true,
+  [boolean]$asp45 = $true,
+
 )
 
 if($downloadpath.EndsWith("\")){
     $computerName.Remove($computerName.LastIndexOf("\"))
 }
 
+Install-WindowsFeature Web-Server, Web-Mgmt-Service -IncludeManagementTools > $null
+
+mkdir $downloadpath
+
+$file = $downloadpath + "\WebDeploy_amd64_en.msi"
+(New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi", $file)
+msiexec /i $file ADDLOCAL=DelegationScriptsFeature /qn /norestart LicenseAccepted="0"
+
+$Acl = Get-Acl "C:\inetpub\wwwroot"
+$Acl.SetAccessRule((New-Object  system.security.accesscontrol.filesystemaccessrule("LOCAL SERVICE","FullControl","Allow")))
+Set-Acl "C:\inetpub\wwwroot" $Acl
+
+[void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.Web.Management")
+[void][Microsoft.Web.Management.Server.ManagementAuthentication]::CreateUser($iisusername, $iispassword)
+[void][Microsoft.Web.Management.Server.ManagementAuthorization]::Grant($iisusername, "Default Web Site", $FALSE)
+
+
+
+if($asp35){
+    Install-WindowsFeature "Web-Asp-Net" > $null
+}
+
+if($asp45){
+    Install-WindowsFeature "Web-Asp-Net45" > $null
+}
+
+
+//
 $instancename = $instancename.ToUpper()
 
 if($tcpportnr -lt 49152 -or $tcpportnr -gt 65535){
     $tcpportnr = 50000
-    echo "Portnumber has to be between 49152 and 65535. Currently set to 50000"
+    echo "Portnumber has to be between 49152 and 65535. Default = 50000"
 }
 
 (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/5/E/9/5E9B18CC-8FD5-467E-B5BF-BADE39C51F73/SQLServer2017-SSEI-Expr.exe", "C:\sqlinstaller.exe")
@@ -28,7 +64,7 @@ Start-Process -FilePath $downloadpath\SQLEXPR_x64_ENU\SETUP.EXE -ArgumentList "/
 
 Import-Module -name 'C:\Program Files (x86)\Microsoft SQL Server\140\Tools\PowerShell\Modules\SQLPS'
 Write-Host "dbname=$dbname"
-Invoke-Sqlcmd -InputFile "C:\vagrant\provisioning\configuresqlserver.sql" -ServerInstance ".\$instancename" -Variable "dbname=$dbname", "login=$username", "password=$password"
+Invoke-Sqlcmd -InputFile "C:\vagrant\provisioning\configuresqlserver.sql" -ServerInstance ".\$instancename" -Variable "dbname=$dbname", "login=$sqlusername", "password=$sqlpassword"
 
 Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQLServer\SuperSocketNetLib\Tcp\IPAll" -name TcpPort -value $tcpportnr
 Restart-Service -Force "MSSQL`$$instancename" > $null
