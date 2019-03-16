634d4f3f668ad399419814e8fea9e19b06f68e0a
Matthias Van De Velde <fpkmatthi0@gmail.com>
2019-03-16 01:41:08 +0100

    Updated testplan + reworked provisioning

--- opdracht02/Documentatie_WISA/Testplan_WISA_Autodeploy.md
+++ opdracht02/Documentatie_WISA/Testplan_WISA_Autodeploy.md
@@ -1,7 +1,7 @@
 # Testplan: Base box Vagrant (WISA)
 *Author: Matthias Van De Velde*
 
-1. Change `blogdemo` to `$true` in vagrant-hosts.yml if it's not already.
+1. Change `blogdemo` to `$true` and `dotnetcore22` to `$true` in vagrant-hosts.yml if it's not already.
 2. `vagrant up wisastack`
 3. Wait for the box to be fully booted and provisioned.
 4. Surf to 192.168.248.10 on your host machine.
--- opdracht02/testomgeving/provisioning/wisastack.ps1
+++ opdracht02/testomgeving/provisioning/wisastack.ps1
@@ -27,13 +27,19 @@ if(!(Test-Path $downloadpath)){
 }
 
 # Install + configure IIS
-Write-Host('Downloading IIS...')
-Install-WindowsFeature Web-Server, Web-Mgmt-Service -IncludeManagementTools > $null
+$serviceWebserver = Get-WindowsFeature Web-Server
+$servicesWebManagement = Get-WindowsFeature Web-Mgmt-Service
+if(!($serviceWebserver).Installed || !($servicesWebManagement).Installed){
+    Write-Host('Downloading IIS...')
+    Install-WindowsFeature Web-Server, Web-Mgmt-Service -IncludeManagementTools > $null
+}else{
+    Write-Host('IIS already isntalled(skipping)')
+}
 
 
 $file = $downloadpath + "\WebDeploy_amd64_en.msi"
 if(!(Test-Path $file)){
-    Write-Host('Installing IIS...')
+    Write-Host('Installing Webdeploy...')
     (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi", $file)
     msiexec /i $file ADDLOCAL=DelegationScriptsFeature /qn /norestart LicenseAccepted="0"
 }
@@ -43,23 +49,36 @@ $Acl.SetAccessRule((New-Object  system.security.accesscontrol.filesystemaccessru
 Set-Acl "C:\inetpub\wwwroot" $Acl
 
 Try{
+    Write-Host('User vagrant already exists(skipping)')
     [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.Web.Management")
     [void][Microsoft.Web.Management.Server.ManagementAuthentication]::CreateUser($iisusername, $iispassword)
     [void][Microsoft.Web.Management.Server.ManagementAuthorization]::Grant($iisusername, "Default Web Site", $FALSE)
 }
 Catch [System.Management.Automation.RuntimeException]{
-
+    Write-Host('User vagrant already exists(skipping)')
 }
 
 # Install ASP.NET
+$serviceAsp35 = Get-WindowsFeature Web-Asp-Net
 if($asp35){
-    Write-Host('Installing .NET Framework support for 3.5 and lower ...')
-    Install-WindowsFeature "Web-Asp-Net" > $null
+    if(!($serviceAsp35).Installed){
+        Write-Host('Installing .NET Framework support for 3.5 and lower ...')
+        Install-WindowsFeature Web-Asp-Net > $null
+    }else{
+        Write-Host('.NET 3.5 Framework already isntalled(skipping)')
+    }
+   
 }
 
+$serviceAsp45 = Get-WindowsFeature Web-Asp-Net45
 if($asp45){
-    Write-Host('Installing .NET Framework support for 4.5 and higher ...')
-    Install-WindowsFeature "Web-Asp-Net45" > $null
+    if(!($serviceAsp45).Installed){
+        Write-Host('Installing .NET Framework support for 4.5 and higher ...')
+        Install-WindowsFeature Web-Asp-Net45 > $null
+    }else{
+        Write-Host('.NET 4.5 Framework already isntalled(skipping)')
+    }
+    
 }
 
 if($dotnetcore21){
@@ -70,11 +89,13 @@ if($dotnetcore21){
         Invoke-WebRequest https://download.visualstudio.microsoft.com/download/pr/dc431217-1692-4db1-9e8b-3512c9788292/3070b595006fadcac1ce3b02aff5fadf/dotnet-hosting-2.1.9-win.exe -OutFile $file
 
         # run installer
-        .\dotnet-hosting-2.1.9-win.exe /install
+        & $file /install
 
         # restart web server
         net stop was /y
         net start w3svc
+    }else{
+        Write-Host('.NET Core 2.1 already isntalled(skipping)')
     }
 }
 
@@ -86,80 +107,54 @@ if($dotnetcore22){
         Invoke-WebRequest https://download.visualstudio.microsoft.com/download/pr/a46ea5ce-a13f-47ff-8728-46cb92eb7ae3/1834ef35031f8ab84312bcc0eceb12af/dotnet-hosting-2.2.3-win.exe -OutFile $file
 
         # run installer
-        .\dotnet-hosting-2.2.3-win.exe /install
+        & $file /install
 
         # restart web server
         net stop was /y
         net start w3svc
+    }else{
+        Write-Host('.NET Core 2.2 already isntalled(skipping)')
     }
 }
 
 
 
 # Install + configure SQL Server
-# Write-Host('Installing SQL Server...')
-# $instancename = $instancename.ToUpper()
+if (Test-Path “HKLM:\Software\Microsoft\Microsoft SQL Server\Instance Names\SQL”) {
+    Write-Host('SQL Server already isntalled(skipping)')
+} else {
+    Write-Host('Installing SQL Server...')
+    $instancename = $instancename.ToUpper()
 
-# if($tcpportnr -lt 49152 -or $tcpportnr -gt 65535){
-#     $tcpportnr = 50000
-#     echo "Portnumber has to be between 49152 and 65535. Default = 50000"
-# }
+    if($tcpportnr -lt 49152 -or $tcpportnr -gt 65535){
+        $tcpportnr = 50000
+        echo "Portnumber has to be between 49152 and 65535. Default = 50000"
+    }
 
-# (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/5/E/9/5E9B18CC-8FD5-467E-B5BF-BADE39C51F73/SQLServer2017-SSEI-Expr.exe", "C:\sqlinstaller.exe")
-# Start-Process -FilePath C:\sqlinstaller.exe -ArgumentList "/action=download /quiet /enu /MediaPath=$downloadpath" -wait
-# Remove-Item C:\sqlinstaller.exe
-# Start-Process -FilePath $downloadpath\SQLEXPR_x64_ENU.exe -WorkingDirectory $downloadpath /q -wait
+    (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/5/E/9/5E9B18CC-8FD5-467E-B5BF-BADE39C51F73/SQLServer2017-SSEI-Expr.exe", "C:\sqlinstaller.exe")
+    Start-Process -FilePath C:\sqlinstaller.exe -ArgumentList "/action=download /quiet /enu /MediaPath=$downloadpath" -wait
+    Remove-Item C:\sqlinstaller.exe
+    Start-Process -FilePath $downloadpath\SQLEXPR_x64_ENU.exe -WorkingDirectory $downloadpath /q -wait
 
-# Start-Process -FilePath $downloadpath\SQLEXPR_x64_ENU\SETUP.EXE -ArgumentList "/Q /Action=install /IAcceptSQLServerLicenseTerms /FEATURES=SQL,Tools /TCPENABLED=1 /SECURITYMODE=`"SQL`" /SQLSYSADMINACCOUNTS=`"BUILTIN\Administrators`" /INSTANCENAME=$instancename /INSTANCEID=$instancename /SAPWD=$rootpassword" -wait
+    Start-Process -FilePath $downloadpath\SQLEXPR_x64_ENU\SETUP.EXE -ArgumentList "/Q /Action=install /IAcceptSQLServerLicenseTerms /FEATURES=SQL,Tools /TCPENABLED=1 /SECURITYMODE=`"SQL`" /SQLSYSADMINACCOUNTS=`"BUILTIN\Administrators`" /INSTANCENAME=$instancename /INSTANCEID=$instancename /SAPWD=$rootpassword" -wait
 
-# Import-Module -name 'C:\Program Files (x86)\Microsoft SQL Server\140\Tools\PowerShell\Modules\SQLPS'
-# Invoke-Sqlcmd -InputFile "C:\vagrant\provisioning\configuresqlserver.sql" -ServerInstance ".\$instancename" -Variable "dbname=$dbname", "login=$sqlusername", "password=$sqlpassword"
+    Import-Module -name 'C:\Program Files (x86)\Microsoft SQL Server\140\Tools\PowerShell\Modules\SQLPS'
+    Invoke-Sqlcmd -InputFile "C:\vagrant\provisioning\configuresqlserver.sql" -ServerInstance ".\$instancename" -Variable "dbname=$dbname", "login=$sqlusername", "password=$sqlpassword"
 
-# Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQLServer\SuperSocketNetLib\Tcp\IPAll" -name TcpPort -value $tcpportnr
-# Restart-Service -Force "MSSQL`$$instancename" > $null
-# New-NetFirewallRule -DisplayName "Allow inbound sqlserver" -Direction Inbound -LocalPort $tcpportnr -Protocol TCP -Action Allow > $null
-
-
-
-if($blogdemo){
-    [string]$sourceDirectory  = "C:\vagrant\provisioning\Blogifier\*"
-    [string]$destinationDirectory = "C:\inetpub\wwwroot\"
-    Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
+    Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQLServer\SuperSocketNetLib\Tcp\IPAll" -name TcpPort -value $tcpportnr
+    Restart-Service -Force "MSSQL`$$instancename" > $null
+    New-NetFirewallRule -DisplayName "Allow inbound sqlserver" -Direction Inbound -LocalPort $tcpportnr -Protocol TCP -Action Allow > $null
 }
 
 
-# if($blogdemo){
-#     # Deploy .net app
-#     Write-Host('Deploying App...')
-#     [string]$username="vagrant"
-#     [string]$downloadlink="https://github.com/rxtur/BlogEngine.NET/releases/download/v3.3.6.0/3360.zip"
-#     [string]$downloadlinkpath="C:\Users\" + $username + "\Documents\aspdotnetapp.zip"
-#     [string]$outpath="C:\inetpub\wwwroot\"
+# Deploy blog demo
+Write-Host('Deploying Blog Demo ...')
+if($blogdemo){
+    if(!($directoryInfo.count -eq 0)){
+        Get-ChildItem -Path C:\Temp -Include * -File -Recurse | foreach { $_.Delete()}
+        $msdeploy = "C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe"
+        & $msdeploy -verb:sync -source:package="C:\vagrant\provisioning\Blogifierpackage\App.zip" -dest:auto
+    }
+}
 
-#     ## Downloading from link
-#     Write-Host("Downloading zip")
-#     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#     Invoke-WebRequest $downloadlink -OutFile $downloadlinkpath
 
-#     ## Delete wwwroot files and folders
-#     Write-Host("clearing wwwroot")
-#     Get-ChildItem -Path C:\inetpub\wwwroot -Include *.* -File -Recurse | foreach {$_.Delete()}
-
-#     ##  Unpacking download
-#     Write-Host("Unpacking zip")
-#     Add-Type -AssemblyName System.IO.Compression.FileSystem
-#     [System.IO.Compression.ZipFile]::ExtractToDirectory($downloadlinkpath, $outpath)
-#     Write-Host("Zip unpacked")
-
-#     ## Setting permissions
-#     $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("IIS_IUSRS", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
-
-#     $AclAppData = Get-Acl "C:\inetpub\wwwroot\App_Data"
-#     $AclCustom = Get-Acl "C:\inetpub\wwwroot\Custom"
-
-#     $AclAppData.AddAccessRule($accessRule)
-#     $AclCustom.AddAccessRule($accessRule)
-
-#     Set-Acl -Path "C:\inetpub\wwwroot\App_Data" -ACLObject $AclAppData
-#     Set-Acl -Path "C:\inetpub\wwwroot\Custom" -ACLObject $AclCustom
-# }
