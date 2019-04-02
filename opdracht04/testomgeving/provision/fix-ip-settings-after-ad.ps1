2898b414d2bd53a4321f0db6fd5f10fd77a35ac6
Matthias Van De Velde <fpkmatthi0@gmail.com>
2019-04-02 23:13:28 +0200

    Fixed adapter name

--- opdracht04/testomgeving/provision/fix-ip-settings-after-ad.ps1
+++ opdracht04/testomgeving/provision/fix-ip-settings-after-ad.ps1
@@ -1,20 +1,21 @@
 # IPv4 settings
-$internalStaticIPv4=192.168.248.10
+$internalStaticIPv4="192.168.248.10"
 $internalPrefixLength=24
 $internalDefaultGateway=""
 
 
 ## Rename adapters
 Write-Host("Renaming adapters to External and Internal...")
-Rename-NetAdapter -Name "Adapter" -NewName "External"
-Rename-NetAdapter -Name "Adapter 2" -NewName "Internal"
+Rename-NetAdapter -Name "Ethernet" -NewName "External"
+Rename-NetAdapter -Name "Ethernet 2" -NewName "Internal"
 
 ## Set external adapter to auto and internal adapter to static
 Write-Host("Fixing External and Internal IP settngs...")
 Get-NetAdapter -Name "External" | Set-NetIPInterface -Dhcp Enabled
-New-NetIPAddress -InterfaceAlias "Internal" -IPv4Address $internalStaticIPv4 -PrefixLength $internalPrefixLength -DefaultGateway $internalDefaultGateway
+Set-NetIPAddress -InterfaceAlias "Internal" -IPAddress $internalStaticIPv4 -PrefixLength $internalPrefixLength
 
 ## Fix external dns
+Write-Host("Fixing External and Internal DNS settngs...")
 Set-DnsClientServerAddress -InterfaceAlias "External" -ResetServerAddresses
 
 ## Fix Internal dns
