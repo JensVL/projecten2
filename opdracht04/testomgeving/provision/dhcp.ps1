2b8d85147768436b199426e6e45b8f0cbe91f302
Matthias Van De Velde <fpkmatthi0@gmail.com>
2019-04-04 20:42:32 +0200

    Fixed dhcp

--- opdracht04/testomgeving/provision/dhcp.ps1
+++ opdracht04/testomgeving/provision/dhcp.ps1
@@ -1,12 +1,13 @@
 # DHCP settings
-$dhcpStartRange=192.168.248.10
-$dhcpEndRange=192.168.248.30
-$dhcpSubnet=255.255.255.0
-$internalStaticIPv4=192.168.248.10
+$dhcpStartRange="192.168.248.10"
+$dhcpEndRange="192.168.248.30"
+$dhcpSubnet="255.255.255.0"
+$internalStaticIPv4="192.168.248.10"
+$scopeid="192.168.248.0"
 
 ## Install DHCP
 Write-Host("Installing DHCP...")
-Install-WindowsFeature -Name "DHCP" –IncludeManagementTools
+Install-WindowsFeature -Name "DHCP" -IncludeManagementTools
 
 ## Make scope for clients
 ### Note: dunno of scopid juist is, of het wel het ip van de internal adapter is
@@ -15,19 +16,19 @@ Add-DhcpServerV4Scope -Name "DHCP Scope" -StartRange $dhcpStartRange -EndRange $
 
 Set-DhcpServerV4OptionValue -DnsServer $internalStaticIPv4 -Router $internalStaticIPv4
 
-Set-DhcpServerv4Scope -ScopeId $internalStaticIPv4 -LeaseDuration 1.00:00:00
+Set-DhcpServerv4Scope -ScopeId $scopeid -LeaseDuration 1.00:00:00
 
 Restart-service dhcpserver
 
-Set-DhcpServerv4Scope -ScopeId $internalStaticIPv4 -State Active
+Set-DhcpServerv4Scope -ScopeId $scopeid -State Active
 
 
 ## Configure DHCP settings for WDS
 ### TODO
-# Write-Host("Configuring DHCP for WDS...")
-# Set-DhcpServerv4OptionValue -ScopeId $internalStaticIPv4 -OptionId 066 -Value $internalStaticIPv4
-# Set-DhcpServerv4OptionValue -ScopeId $internalStaticIPv4 -OptionId 067 -Value "boot\x64\wdsnbp.com"
-# Set Domain Name Set-DhcpServerv4OptionValue -ScopeId $internalStaticIPv4 -OptionId 015 -Value "example.com"
-# Set-DhcpServerv4OptionValue -ScopeId $internalStaticIPv4 -DnsServer $internalStaticIPv4
+Write-Host("Configuring DHCP for WDS...")
+Set-DhcpServerv4OptionValue -ScopeId $scopeid -OptionId 066 -Value $internalStaticIPv4
+Set-DhcpServerv4OptionValue -ScopeId $scopeid -OptionId 067 -Value "boot\x64\wdsnbp.com"
+Set Domain Name Set-DhcpServerv4OptionValue -ScopeId $scopeid -OptionId 015 -Value "example.com"
+Set-DhcpServerv4OptionValue -ScopeId $scopeid -DnsServer $internalStaticIPv4
 
 
