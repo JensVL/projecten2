6cdefe0b2b90cc445b466200e408140c10d4f443
Matthias Van De Velde <fpkmatthi0@gmail.com>
2019-05-04 21:41:00 +0200

    Changed hostname for netbios name error and edited paths for wds

--- opdracht04/testomgeving/provision/wds.ps1
+++ opdracht04/testomgeving/provision/wds.ps1
@@ -5,52 +5,54 @@
 ## Exception setting "Path": "Unable to cast object of type 'System.IO.DirectoryInfo' to type 'Microsoft.BDD.PSSnapIn.MDTObject'."
 ## mabe with [string]::Format('{0}',$isodrive) -> for now testing with hardcoded driveletter
 
-Write-Host("Installing WDS...")
-try { 
-    Write-Host "Imports the Windows Deployment Services PowerShell module"
-    Install-WindowsFeature wds-deployment -includemanagementtools
-    Write-Host "- Windows Deployment Services PowerShell module imported"
-} catch {
-    Write-Warning "- Could not import the Windows Deployment Services PowerShell module from first location"
-    Write-Warning "- Trying to Import the WDS PowerShell module again"
-    try {
-        Write-Host "Imports the Windows Deployment Services PowerShell module"
-        Install-WindowsFeature wds-deployment -includemanagementtools
-            Write-Host "- Windows Deployment Services PowerShell module imported"
-    } catch {
-        Write-Warning "- Could not import the Windows Deployment Services PowerShell module"
-        Break
-    }
-}
+# Write-Host("Installing WDS...")
+# try { 
+#     Write-Host "Imports the Windows Deployment Services PowerShell module"
+#     Install-WindowsFeature wds-deployment -includemanagementtools
+#     Write-Host "- Windows Deployment Services PowerShell module imported"
+# } catch {
+#     Write-Warning "- Could not import the Windows Deployment Services PowerShell module from first location"
+#     Write-Warning "- Trying to Import the WDS PowerShell module again"
+#     try {
+#         Write-Host "Imports the Windows Deployment Services PowerShell module"
+#         Install-WindowsFeature wds-deployment -includemanagementtools
+#             Write-Host "- Windows Deployment Services PowerShell module imported"
+#     } catch {
+#         Write-Warning "- Could not import the Windows Deployment Services PowerShell module"
+#         Break
+#     }
+# }
+
+$InstallDrive = "E:" 
 
 
 Write-Host("Initializing WDS...")
-try {
+# try {
     # Change dirs to folder that is used in mdt.ps1
-    $wdsUtilResults = wdsutil /initialize-server /server:example.com /remInst:"E:\RemInstall"
-    $wdsUtilResults | select -last 1
-} catch {
-    Write-Warning "- Could not initialize the Windows Deployment Services PowerShell module"
-}
+    $wdsUtilResults = wdsutil.exe /initialize-server /server:example.com /remInst:"E:\MDTProduction$"
+    $wdsUtilResults
+# } catch {
+    # Write-Warning "- Could not initialize the Windows Deployment Services PowerShell module"
+# }
 
 
 Write-Host("Adding Boot image WDS Server...")
-try {
-    # # location of .wim file created in mdt.ps1
-    # Import-WdsBootImage -Path "D:\sources\boot.wim"
-} catch {
-    Write-Warning "- Could not initialize the Windows Deployment Services PowerShell module"
-}
+# try {
+    # location of .wim file created in mdt.ps1
+    Import-WdsBootImage -Path "D:\MDTProduction\Operating Systems\Windows10\sources\boot.wim"
+# } catch {
+    # Write-Warning "- Could not initialize the Windows Deployment Services PowerShell module"
+# }
 
 Write-Host("Importing the WDS Install Image...")
-try{
+# try{
     New-WdsInstallImageGroup -Name "desktops" 
-    # # Change dirs to the ones used in mdt.ps1
-    # Get-WindowsImage -imagePath "D:\sources\install.wim" | select Imagename 
+    # Change dirs to the ones used in mdt.ps1
+    Get-WindowsImage -imagePath "D:\MDTProduction\Operating Systems\Windows10\sources\install.wim" | select Imagename 
     $imageName = 'Windows 10 Pro' 
-    # Import-WdsInstallImage -ImageGroup "desktops" -Path "D:\sources\install.wim" -ImageName $imageName
-} catch {
-    Write-Warning "- Could not initialize the Windows Deployment Services PowerShell module"
-}
+    Import-WdsInstallImage -ImageGroup "desktops" -Path "D:\MDTProduction\Operating Systems\Windows10\sources\install.wim" -ImageName $imageName
+# } catch {
+#     Write-Warning "- Could not initialize the Windows Deployment Services PowerShell module"
+# }
 
 Write-Host("Installed WDS")
